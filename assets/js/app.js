9ccc471f4fd4e3ff609f94a0a7af2b23adee7de9
casee8 <pokerkaszy7@gmail.com>
2018-12-12 13:43:59 +0000

    Page loading optimisation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,39 @@
 const menuBtn = document.querySelector('#menuBtn');
 const menuContent = document.querySelector('.menu');
-const navBar = document.querySelector('nav');
 const main = document.querySelector('main');
 
-menuBtn.addEventListener('click', function(){
-  $(menuContent).toggleClass('show');
-});
+function toggleMenu(){
+  menuContent.classList.toggle('show');
+};
 
-main.addEventListener('click', function(){
+menuBtn.addEventListener("click", toggleMenu, false);
+
+// Close the menu from anywhere
+function removeShow(){
   menuContent.classList.remove('show');
-});
+};
+
+main.addEventListener('click', removeShow, false);
+
+
+// =================== Image Loading Optimisation ===================
+// (() => {
+//   if(window.innerWidth > 599) {
+//     'use strict';
+//     // Page is loaded
+//     const objects = document.getElementsByClassName('asyncImage');
+//     Array.from(objects).map((item) => {
+//       // Start loading image
+//       const img = new Image();
+//       img.src = item.dataset.src;
+//       // Once image is loaded replace the src of the HTML element
+//       img.onload = () => {
+//         item.classList.remove('asyncImage');
+//         return item.nodeName === 'IMG' ? 
+//           item.src = item.dataset.src :        
+//           item.style.backgroundImage = `url(${item.dataset.src})`;
+//       };
+//     });
+//   }
+// })();
+// ==================================================================
