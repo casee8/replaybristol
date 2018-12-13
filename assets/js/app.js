039c55c6dae892271315aad92aa539e67f9b93e8
casee8 <pokerkaszy7@gmail.com>
2018-12-13 17:12:30 +0000

    Added effects to images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
 const menuBtn = document.querySelector('#menuBtn');
 const menuContent = document.querySelector('.menu');
 const main = document.querySelector('main');
+const allImg = document.querySelectorAll('.all-img');
+
+function imgFilter(){
+  for (var i = 0; i < allImg.length; i++) {
+    allImg[i].classList.add('turn-to-normal');
+  }
+}
+
+setTimeout(imgFilter, 100);
 
 function toggleMenu(){
   menuContent.classList.toggle('show');
