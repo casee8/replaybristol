987706aeaa1172268516bd4f572e733f4edf099d
casee8 <pokerkaszy7@gmail.com>
2018-12-19 13:08:38 +0000

    Few style changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,30 @@
-const menuBtn = document.querySelector('#menuBtn');
-const menuContent = document.querySelector('.menu');
-const main = document.querySelector('main');
-const allImg = document.querySelectorAll('.all-img');
+(function() {
+  const menuBtn = document.querySelector('#menuBtn');
+  const menuContent = document.querySelector('.menu');
+  const main = document.querySelector('main');
+  const allImg = document.querySelectorAll('.all-img');
 
-function imgFilter(){
-  for (var i = 0; i < allImg.length; i++) {
-    allImg[i].classList.add('turn-to-normal');
+  function imgFilter(){
+    for (var i = 0; i < allImg.length; i++) {
+      allImg[i].classList.add('turn-to-normal');
+    }
   }
-}
 
-setTimeout(imgFilter, 100);
+  setTimeout(imgFilter, 100);
 
-function toggleMenu(){
-  menuContent.classList.toggle('show');
-};
+  function toggleMenu(){
+    menuContent.classList.toggle('show');
+  };
 
-menuBtn.addEventListener("click", toggleMenu, false);
+  menuBtn.addEventListener("click", toggleMenu, false);
 
-// Close the menu from anywhere
-function removeShow(){
-  menuContent.classList.remove('show');
-};
+  // Close the menu from anywhere
+  function removeShow(){
+    menuContent.classList.remove('show');
+  };
 
-main.addEventListener('click', removeShow, false);
+  main.addEventListener('click', removeShow, false);
+})();
 
 
 // =================== Image Loading Optimisation ===================
