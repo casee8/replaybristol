796fc8d0a510a233e239613d8b812290b1bb889d
casee8 <pokerkaszy7@gmail.com>
2019-04-17 10:30:51 +0100

    New background, small fixings

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,19 +3,35 @@ const menuContent = document.querySelector('.menu');
 const main = document.querySelector('main');
 const allImg = document.querySelectorAll('.all-img');
 
+// Store the menu status (0 = closed)
+let checkNum = 0;
+
 function mainApp() {
-  function toggleMenu(){
-    menuContent.classList.toggle('show');
+  function openMenu(){
+    menuContent.style.visibility = "visible";
+    menuContent.style.transform = "translate3d(0, 0, 0)";
+    menuContent.style.transition = "all 500ms ease";
+    checkNum = 1;
   };
-
-  menuBtn.addEventListener("click", toggleMenu, false);
-
+  
+  function closeMenu(){
+    menuContent.style.visibility = "hidden";
+    menuContent.style.transform = "translate3d(-100%, 0, 0)";
+    menuContent.style.transition = "all 500ms ease";
+    checkNum = 0;
+  };
+  
+  // Check the menu status
+  menuBtn.addEventListener("click", function() {
+    if(checkNum == 0) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  });
+  
   // Close the menu from anywhere
-  function removeShow(){
-    menuContent.classList.remove('show');
-  };
-
-  main.addEventListener('click', removeShow, false);
+  main.addEventListener('click', closeMenu, false);
 };
 
 mainApp();
